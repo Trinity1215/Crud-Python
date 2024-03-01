d719a610603767902a46286b5cee14262db0e95f
Cristian <cdbeltran25@misena.edu.co>
2024-02-29 21:46:07 -0500

    Tabla Garantias

--- Crud_Python/etrdjango/bicicleteria/admin.py
+++ Crud_Python/etrdjango/bicicleteria/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Bicicleta
+from .models import Garantia
 # Register your models here.
 admin.site.register(Bicicleta)
+admin.site.register(Garantia)
