56d60fa3ef5cbd1676e92106f8677b0e22799923
Cristian <cdbeltran25@misena.edu.co>
2024-04-03 14:18:50 -0500

    Campos

--- Crud_Python/etrdjango/bicicleteria/admin.py
+++ Crud_Python/etrdjango/bicicleteria/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Bicicleta
 from .models import Garantia
+from .models import Marca
 # Register your models here.
 admin.site.register(Bicicleta)
 admin.site.register(Garantia)
--- Crud_Python/etrdjango/bicicleteria/models.py
+++ Crud_Python/etrdjango/bicicleteria/models.py
@@ -19,18 +19,28 @@ class EstadoBicicleta(models.Model):
 
     def __str__(self):
         return self.nombre
+    
 
+class Marca(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name='Nombre de la marca')
+    id = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return self.nombre
+    
 class Bicicleta(models.Model):
     id = models.AutoField(primary_key=True)
-    marca = models.CharField(max_length=100, verbose_name='Marca')
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE, verbose_name='Marca')
     imagen = models.ImageField(upload_to='imagenes/', verbose_name='Imagen', null=True)
     descripcion = models.TextField(verbose_name='Descripcion', null=True)
     precio = models.DecimalField(max_digits=10, decimal_places=3, validators=[MinValueValidator(0)], verbose_name='Precio')
+    mano_de_obra = models.DecimalField(max_digits=10, decimal_places=3, validators=[MinValueValidator(0)], verbose_name='Valor de mano de obra', null=True, blank=True)
     fecha = models.DateField(default=timezone.now, verbose_name='Fecha de ingreso')
     estado = models.ForeignKey(EstadoBicicleta, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return f"Marca: {self.marca} - Descripcion: {self.descripcion}"
+        return f"Marca: {self.marca.nombre} - Descripcion: {self.descripcion}"
+
 
     def fecha_terminacion_garantia(self):
         if self.estado.nombre == EstadoBicicleta.TERMINADO: 
